7dbfaf68539be808d8341be325ca8afb3c236699
Ian Pelakh <ispelakh@iastate.edu>
2023-11-23 19:55:53 -0600

    test

---
+++ job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Submit this script with: sbatch thefilename
+#SBATCH --time=0-1:00:00  # max job runtime
+#SBATCH --cpus-per-task=1  # number of processor cores
+#SBATCH --nodes=1  # number of nodes
+#SBATCH --partition=legion  # partition(s)
+#SBATCH --mem=10G  # max memory
+#SBATCH -J "mnist-test"  # job name
+
+./main.o
