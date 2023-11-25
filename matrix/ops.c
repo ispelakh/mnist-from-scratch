84237aaf212b91f41e26f40c9e6f12a0125521ad
Ian Pelakh <ispelakh@iastate.edu>
2023-11-24 19:41:29 -0600

    test

--- matrix/ops.c
+++ matrix/ops.c
@@ -1,7 +1,7 @@
 #include "ops.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define NUM_THREADS 1
+#define NUM_THREADS 8
 
 int check_dimensions(Matrix *m1, Matrix *m2) {
 	if (m1->rows == m2->rows && m1->cols == m2->cols) return 1;
